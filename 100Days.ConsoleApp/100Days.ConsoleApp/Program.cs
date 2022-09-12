0c750d9c3aafacc0db675c20a278a9dfa4eae795
Tirth <tirthacodes@gmail.com>
2022-09-12 21:39:58 +0545

    Created a ConsoleApp Project to practice C#

---
+++ 100Days.ConsoleApp/100Days.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _100Days.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ControlStatementExample();
+
+        }
+
+        static void ControlStatementExample()
+        {
+            Console.WriteLine("Enter the number of day:");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
