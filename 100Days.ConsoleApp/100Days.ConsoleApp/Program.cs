b0e2cfea0f4fca5bb40d8332d44ec285160a7c3e
Tirth <tirthacodes@gmail.com>
2022-09-12 22:06:54 +0545

    code to enter a number to print a day added

--- 100Days.ConsoleApp/100Days.ConsoleApp/Program.cs
+++ 100Days.ConsoleApp/100Days.ConsoleApp/Program.cs
@@ -14,6 +14,34 @@
             Console.WriteLine("Enter the number of day:");
             var num = Convert.ToInt32(Console.ReadLine());
 
+            if (num==1)
+            {
+                Console.WriteLine("sunday");
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("monday");
+            }
+            else if (num == 3)
+            {
+                Console.WriteLine("tuesday");
+            }
+            else if (num == 4)
+            {
+                Console.WriteLine("wednesday");
+            }
+            else if (num == 5)
+            {
+                Console.WriteLine("thursday");
+            }
+            else if (num == 6)
+            {
+                Console.WriteLine("friday");
+            }
+            else if (num == 7) 
+            {
+                Console.WriteLine("saturday");
+            }
 
         }
     }
