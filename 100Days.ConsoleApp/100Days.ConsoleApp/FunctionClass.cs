0da12acd4cd61e0531de83c65caa269d954b0648
Tirth <tirthacodes@gmail.com>
2022-11-10 23:02:42 +0545

    implemented few function overloading

--- 100Days.ConsoleApp/100Days.ConsoleApp/FunctionClass.cs
+++ 100Days.ConsoleApp/100Days.ConsoleApp/FunctionClass.cs
@@ -8,5 +8,20 @@
         }
 
         public int AddV2(int a, int b) => a + b;
+        public int Add(int x, int y, int z) => x + y + z;
+        public void FuncOne(int x, string y)
+        {
+
+        }
+
+        public void FuncOne(string y, int x)
+        {
+
+        }
+
+        public void FuncTwo(int a, string b)
+        {
+
+        }
     }
 }
