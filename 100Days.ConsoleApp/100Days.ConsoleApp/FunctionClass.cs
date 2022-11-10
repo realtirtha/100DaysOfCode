70ff514dc1b339fbc371fd6877effba3609b824d
Tirth <tirthacodes@gmail.com>
2022-11-10 22:56:19 +0545

    created class FunctionClass

---
+++ 100Days.ConsoleApp/100Days.ConsoleApp/FunctionClass.cs
@@ -0,0 +1,12 @@
+﻿namespace _100Days.ConsoleApp
+{
+    public class FunctionClass
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int AddV2(int a, int b) => a + b;
+    }
+}
