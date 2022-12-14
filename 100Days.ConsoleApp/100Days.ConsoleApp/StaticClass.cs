c118a121346266852b0f7b1daae6374c0d35a2d0
Tirth <tirthacodes@gmail.com>
2022-12-14 16:09:26 +0545

    created static and non static classes

---
+++ 100Days.ConsoleApp/100Days.ConsoleApp/StaticClass.cs
@@ -0,0 +1,19 @@
+﻿namespace _100Days.ConsoleApp
+{
+    public static class StaticClass
+    {
+        //everything here must be static
+        public static int i = 10;
+        public static void FunctionOne()
+        {
+
+        }
+    }
+
+    public class NonStaticClass
+    {
+        //may or may not be static
+        public int i = 10;
+
+    }
+}
