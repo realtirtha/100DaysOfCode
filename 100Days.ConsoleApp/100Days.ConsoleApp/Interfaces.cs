b420acbfcc0720061b787f9b7bb451539023a076
Tirth <tirthacodes@gmail.com>
2022-12-20 15:15:56 +0545

    ABC, Interfaces and Interface class created

---
+++ 100Days.ConsoleApp/100Days.ConsoleApp/Interfaces.cs
@@ -0,0 +1,44 @@
+﻿namespace _100Days.ConsoleApp
+{
+    public interface IInterfaces
+    {
+        int Add(int a, int b);
+        void Add();
+
+        public void Test()
+        {
+            Console.WriteLine("abc");
+        }
+    }
+    public class ABC : IInterfaces
+    {
+        public int Add(int a, int b)
+        {
+            return 0;
+        }
+        public void Add()
+        {
+            //something
+        }
+        public void Testing()
+        {
+
+        }
+    }
+
+    public class Interface : IInterfaces
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public void Add()
+        {
+            //something
+        }
+        private void ABCTest()
+        {
+
+        }
+    }
+}
