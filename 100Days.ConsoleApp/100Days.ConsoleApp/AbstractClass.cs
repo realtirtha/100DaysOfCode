0601867f6c2b5e342174cadf5972aacdc5a7d5e5
Tirth <tirthacodes@gmail.com>
2022-12-27 23:10:17 +0545

    created abstract Ashape class

--- 100Days.ConsoleApp/100Days.ConsoleApp/AbstractClass.cs
+++ 100Days.ConsoleApp/100Days.ConsoleApp/AbstractClass.cs
@@ -3,12 +3,17 @@
     public abstract class AbstractClass
     {
         public void FunctionOne()
-        {
+        { 
            Console.WriteLine("I'm from function one");
         }
         protected abstract void FunctionTwo();
         
         
         
+    }
+
+    public abstract class AShape 
+    {
+
     }
 }
