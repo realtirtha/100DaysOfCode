7d4fa2853b0c756867f693cbbe91a27f3f89b7ce
Tirth <tirthacodes@gmail.com>
2022-12-27 23:23:39 +0545

    data members and function added

--- 100Days.ConsoleApp/100Days.ConsoleApp/AbstractClass.cs
+++ 100Days.ConsoleApp/100Days.ConsoleApp/AbstractClass.cs
@@ -14,6 +14,39 @@
 
     public abstract class AShape 
     {
+        protected double area;
+        protected double perimeter;
+        
+        public void Area()
+        {
+            Console.WriteLine($"Area =  {area}");
+        }
+        public void Perimeter()
+        {
+            Console.WriteLine($"Perimeter =  {Perimeter}");
+        }
+        public void GetInput()
+        {
+            getInput();
+            calculate();
+        }
 
+        protected abstract void getInput();
+        protected abstract void calculate();
+    }
+    public class ASquare : AShape
+    {
+        private double length;
+        protected override void calculate()
+        {
+            area = Math.Pow(length, 2);
+            perimeter = 4 * length;
+        }
+
+        protected override void getInput()
+        {
+            Console.WriteLine("Enter the length");
+            length = Convert.ToDouble(Console.ReadLine());
+        }
     }
 }
